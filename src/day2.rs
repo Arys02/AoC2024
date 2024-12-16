2469b0c250351fa0659bd055d976aebbe2b52cf3
arys <charles.yaiche.lf@gmail.com>
2024-12-16 10:09:59 +0100

    D2

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,4 @@
-use std::collections::HashMap;
-
-fn check_safe(report: &str) -> usize {
-    let report : Vec<usize>= report.split(" ").flat_map(|x| { x.parse::<usize>()}).collect();
-
+fn check_safe(report: Vec<usize>) -> usize {
     let is_desc = report[0] > report[1];
 
     for i in 1..report.len() {
@@ -23,34 +19,12 @@ fn check_safe(report: &str) -> usize {
     1
 }
 
-fn check_safe2(report: &str) -> usize {
-    let report : Vec<usize>= report.split(" ").flat_map(|x| { x.parse::<usize>()}).collect();
-
-    let is_desc = report[0] > report[1];
-
-    for i in 1..report.len() {
-        let mut nb_error = 0;
-        if report[i - 1] == report[i] {
-            nb_error += 1;
-        }
-        if is_desc && report[i - 1] < report[i] {
-            nb_error += 1;
-        }
-        if !is_desc && report[i - 1] > report[i] {
-            nb_error += 1;
-        }
-        if report[i].abs_diff(report[i - 1]) > 3 {
-            nb_error += 1;
-        }
-    }
-
-    1
-}
 
 pub fn part1(input: &str) -> usize {
     let mut ret = 0;
     for report in input.split("\r\n").into_iter() {
-        ret += check_safe(report);
+        let report_v: Vec<usize> = report.split(" ").flat_map(|x| x.parse::<usize>()).collect();
+        ret += check_safe(report_v);
     }
     ret
 }
@@ -58,7 +32,29 @@ pub fn part1(input: &str) -> usize {
 pub fn part2(input: &str) -> usize {
     let mut ret = 0;
     for report in input.split("\r\n").into_iter() {
-        ret += check_safe(report);
+
+        let report_v: Vec<usize> = report.split(" ").flat_map(|x| x.parse::<usize>()).collect();
+        let len = report_v.len();
+        let is_safe = check_safe(report_v.clone());
+
+        if is_safe == 0 {
+            for i in 0..len {
+                let mut clone_report = report_v.clone();
+                clone_report.remove(i);
+
+                let is_unsafe_without_i = check_safe(clone_report);
+                if is_unsafe_without_i == 1 {
+                    ret += 1;
+                    break;
+                }
+
+            }
+
+        }
+        else if is_safe == 1 {
+            ret += 1
+        }
+
     }
     ret
 }
@@ -78,20 +74,19 @@ mod tests {
     pub fn test_part1_final() {
         let input_string = include_str!("../input/2024/day2.txt");
         let result = part1(input_string);
-        print!("Final value: {}\n", result);
-        panic!("lol");
+        panic!("{}", result);
     }
     #[test]
     pub fn test_part2() {
-        let x = "3   4\r\n4   3\r\n2   5\r\n1   3\r\n3   9\r\n3   3";
-        assert_eq!(part2(x), 31);
+        let x = "7 6 4 2 1\r\n1 2 7 8 9\r\n9 7 6 2 1\r\n1 3 2 4 5\r\n8 6 4 4 1\r\n1 3 6 7 9";
+        assert_eq!(part2(x), 4);
     }
 
     #[test]
+    #[should_panic()]
     pub fn test_part2_final() {
-        let input_string = include_str!("../input/2024/day1.txt");
+        let input_string = include_str!("../input/2024/day2.txt");
         let result = part2(input_string);
-        print!("Final value: {}\n", result);
+        panic!("{}", result);
     }
-
 }
